49ed0f7f22e3cfdf46ce02d553e9cd421062163b
Pedro Coelho <pedrocoelho.nascimento@gmail.com>
2025-02-05 23:53:01 -0300

    [19] Create error handling page
    * Create error handling page

---
+++ packages/nextjs/app/error.tsx
@@ -0,0 +1,48 @@
+"use client"; // Must be a client component
+
+import { useEffect } from "react";
+import Button from "~~/components/Button";
+import { classes } from './error.styles';
+
+export default function GlobalError({
+    error,
+    reset,
+}: {
+    error: Error;
+    reset: () => void;
+}) {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div className={classes.container}>
+            <h1 className={classes.title}>
+                500: <small className={classes.subtitle}>System Failure</small>
+            </h1>
+
+            <p className={classes.errorMessage}>
+                {`Something exploded. We're investigating. In the meantime, try not to break anything else.`}
+            </p>
+
+            <div className={classes.errorContainer}>
+                <p>While we sweep up the digital debris, here are some possible causes:</p>
+                <ul className={classes.errorList}>
+                    <li>{`A developer pushed 'one last change' on a Friday.`}</li>
+                    <li>The server overheated from all the awesome.</li>
+                    <li>An AI tried to divide by zero.</li>
+                </ul>
+            </div>
+
+            <p className={classes.errorSuggestion}>{`But don't panic (yet). Try one of these:`}</p>
+
+            <div className={classes.errorActions}>
+                <p className="text-lg lg:text-2xl font-medium m-0 space-x-4">
+                    <a href="/" className={classes.actionLink}>Try Again</a> |
+                    <a href="/" className={classes.actionLink}>Flee to Safety</a> |
+                    <a href="/" className={classes.actionLink}>Sound the alarm</a>
+                </p>
+            </div>
+        </div>
+    );
+}
