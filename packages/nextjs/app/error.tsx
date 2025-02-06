a09a85ba43052cb222a989d39a55435900ec044d
pedroco3lho <pedrocoelho.nascimento@gmail.com>
2025-02-05 21:06:32 -0300

    fix: eslint rules

--- packages/nextjs/app/error.tsx
+++ packages/nextjs/app/error.tsx
@@ -22,19 +22,19 @@ export default function GlobalError({
             </h1>
 
             <p className={classes.errorMessage}>
-                Something exploded. We're investigating. In the meantime, try not to break anything else.
+                {`Something exploded. We're investigating. In the meantime, try not to break anything else.`}
             </p>
 
             <div className={classes.errorContainer}>
                 <p>While we sweep up the digital debris, here are some possible causes:</p>
                 <ul className={classes.errorList}>
-                    <li>A developer pushed 'one last change' on a Friday.</li>
+                    <li>{`A developer pushed 'one last change' on a Friday.`}</li>
                     <li>The server overheated from all the awesome.</li>
                     <li>An AI tried to divide by zero.</li>
                 </ul>
             </div>
 
-            <p className={classes.errorSuggestion}>But don't panic (yet). Try one of these:</p>
+            <p className={classes.errorSuggestion}>{`But don't panic (yet). Try one of these:`}</p>
 
             <div className={classes.errorActions}>
                 <p className="text-lg lg:text-2xl font-medium m-0 space-x-4">
