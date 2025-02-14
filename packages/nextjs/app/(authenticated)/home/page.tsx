73c3ca1e8a4d245c41cef5e139765ab866568313
Verifieddanny <dannyclassic56@gmail.com>
2025-02-14 21:51:45 +0100

    feat(home-page): Update home page to reflect latest Figma designs

--- packages/nextjs/app/(authenticated)/home/page.tsx
+++ packages/nextjs/app/(authenticated)/home/page.tsx
@@ -1,13 +1,40 @@
+"use client"
+import { formatLocalTime, truncateAddress } from "~~/utils/format";
 import { classes } from "./page.styles";
+import { useAccount, useConnect } from "@starknet-react/core";
+import { useEffect, useState } from "react";
+
 
 const Home = () => {
+  const { address } = useAccount();
+  const {connector } = useConnect(); 
+  const [currentTime, setCurrentTime] = useState<string>("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const formattedTime = formatLocalTime(now);
+      setCurrentTime(formattedTime);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className={classes.container}>
-      <div className="px-5">
-        <h1 className="text-center">
-          <span className={classes.title}>Netstrike Home</span>
-        </h1>
+      <div className={classes.header}>
+        <p>Hacker &gt;&gt;&gt; NaN0ck |  ADDRESS &gt;&gt;&gt; {truncateAddress(address as string)} | Wallet &gt;&gt;&gt; {connector?.name} </p>
+        <p>{currentTime}</p>
       </div>
+    
+    <div className="flex  font-medium text-[#24DC8F1A] items-baseline">
+      <p className="text-8xl">NETSTRIKE</p>
+      <p className="text-4xl">v1.0</p>
+    </div>
+
+      <div className="text-[#000000] absolute w-[95vw] bottom-[1.125rem] text-xl mx-auto bg-[#24DC8F] flex justify-center py-2 px-[1.8rem] items-center h-[3.75rem] font-medium">
+        <p>HELP | 96008N1 8N1 | NOR | NS OS 0.1 | VT102 | Online | ttyACM0</p>
+      </div>
+
     </div>
   );
 };
---
+++ packages/nextjs/utils/format.ts
@@ -0,0 +1,23 @@
+export const truncateAddress = (address: string, startLength = 6, endLength = 6): string => {
+    if (!address) return "";
+  
+    if (address.length <= startLength + endLength) return address;
+  
+    return `${address.slice(0, startLength)}...${address.slice(-endLength)}`;
+  };
+
+export const formatLocalTime = (date: Date): string => {
+    const year = date.getFullYear().toString().slice(-2); 
+    const month = String(date.getMonth() + 1).padStart(2, "0"); 
+    const day = String(date.getDate()).padStart(2, "0"); 
+  
+    let hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, "0"); 
+    const period = hours >= 12 ? "pm" : "am"; 
+  
+
+    hours = hours % 12 || 12; 
+  
+ 
+    return `${year}-${month}-${day} ${hours}:${minutes}${period}`;
+  };
